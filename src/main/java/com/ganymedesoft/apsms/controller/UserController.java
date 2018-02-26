896368c68e4725eeb9e4c926b9a5440bc546c999
GLADIATORSSOFT <36370627+GLADIATORSSOFT@users.noreply.github.com>
2018-02-26 06:23:31 -0700

    Add files via upload

---
+++ src/main/java/com/ganymedesoft/apsms/controller/UserController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ganymedesoft.apsms.controller;
+
+/**
+ *
+ * @author adeel.usmani
+ */
+public class UserController {
+    
+}
